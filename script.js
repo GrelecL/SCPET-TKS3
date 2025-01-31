38e30e5c61de2b97f4263c9a1c456e5949853c5d
Lovro Grilc <lovrogrilc@gmail.com>
2025-01-31 12:39:33 +0100

    Dodano učenje JavaScripta, je v script.js fajlu z funkcijami lahko kličemo, mora biti vključen v headerju kjer mislimo uporabljati.

---
+++ script.js
@@ -0,0 +1,9 @@
+function mojafunkcija() {
+    alert("Pozdrav iz funkcije!");
+    }
+
+function vnesipodatke() {
+    var ime = prompt("Vnesi svoje ime:");
+    var priimek = prompt("Vnesi svoj priimek:");
+    alert("Pozdravljen, " + ime + " " + priimek + "!");
+    }
